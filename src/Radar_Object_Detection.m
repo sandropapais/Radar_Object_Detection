2038d2e1399916a0593e5d5e826969d636081dd7
sandropapais <30631115+sandropapais@users.noreply.github.com>
2021-02-18 11:11:07 -0800

    removed signal processing toolbox dependence

--- src/Radar_Object_Detection.m
+++ src/Radar_Object_Detection.m
@@ -185,7 +185,7 @@ noise_level = 0;
 
 %Use RDM[x,y] as the matrix from the output of 2D FFT for implementing CFAR
 
-RDM_pow = db2pow(RDM);
+RDM_pow = 10.^(RDM/10);
 
 for i = Tr + Gr + 1:(Nr/2) - (Tr + Gr)
     for j = Td + Gd + 1:Nd - (Td + Gd)
@@ -232,4 +232,28 @@ colorbar;
 view(0,-90)
 xlabel('velocity estimate (m/s)')
 ylabel('range estimate (m)')
+
+
+
+function ydB = pow2db(y)
+%POW2DB   Power to dB conversion
+%   YDB = POW2DB(Y) convert the data Y into its corresponding dB value YDB
+%
+%   % Example:
+%   %   Calculate ratio of 2000W to 2W in decibels
+%
+%   y1 = pow2db(2000/2)     % Answer in db
+
+%#codegen
+cond = all(y(:)>=0);
+if ~cond
+    coder.internal.assert(cond,'signal:pow2db:InvalidInput');
+end
+
+% We want to guarantee that the result is an integer
+% if y is a negative power of 10.  To do so, we force
+% some rounding of precision by adding 300-300.
+
+ydB = (10.*log10(y)+300)-300;
+end
 
